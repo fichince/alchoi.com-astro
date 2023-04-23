d5a898ee8d84200886f07d8fbe25bda813a8cfa3
fichince <acchoi@gmail.com>
2023-04-23 14:58:35 -0400

    Use image optimization in blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,12 +12,13 @@ export default defineConfig({
     tailwind(), 
     image({
       serviceEntryPoint: '@astrojs/image/sharp'
-    })
+    }), 
+    mdx()
   ],
   markdown: {
     shikiConfig: {
       theme: 'vitesse-dark',
-      wrap: true,
+      wrap: true
     }
   }
 });
