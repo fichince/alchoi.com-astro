78839a003d2583bf529ccd6146d27ccf5d054409
fichince <acchoi@gmail.com>
2023-05-19 11:17:57 -0600

    toggling and applying theme

---
+++ src/components/ThemeToggler.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { getTheme, toggleTheme } from '@src/scripts/theme';
+
+  let theme : string;
+
+  onMount(() => {
+    theme = getTheme();
+  });
+</script>
+
+<button class="button" on:click={() => theme = toggleTheme()}>
+  { theme === 'light' ? 'Go Dark' : 'Go Light' }
+</button>
