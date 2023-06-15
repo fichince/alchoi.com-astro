84bb22a817868e46521a70d0d5834ccfe1a4763a
fichince <acchoi@gmail.com>
2023-06-15 11:56:06 -0400

    Light mode (#22)
    * toggling and applying theme
    
    * defining light theme... WIP
    
    * added light palette to CSS
    
    * Tweaking light theme
    
    * working toggle button for light mode, just need to change the sizes
    
    * comment
    
    * adjust size of dark mode toggler
    
    * only render analytics tag in prod
    
    * properly use fragment
    
    * fix layout for toggler
    
    * update usage of colours (WIP)
    
    * a bunch of colour stuff
    
    * more tweaks
    
    * wip
    
    * WIP
    
    * changing remainder of accent colour
    
    * cleanup
    
    * I can't release the light theme until I deal with the FOUC

---
+++ src/scripts/theme.ts
@@ -0,0 +1,32 @@
+export function applyTheme(theme : string) {
+  // we're dark by default, but shoelace is light by default...
+  // hence a little bit of awkwardness here
+  if (theme === 'light') {
+    document.documentElement.classList.add('light');
+    document.documentElement.classList.remove('sl-theme-dark');
+  } else {
+    document.documentElement.classList.remove('light');
+    document.documentElement.classList.add('sl-theme-dark');
+  }
+}
+
+export function getTheme() {
+  let theme = localStorage.getItem('theme');
+  if (!theme) {
+    // initialize theme based on system preference
+    let prefersLight = window.matchMedia('(prefers-color-scheme: light)').matches;
+
+    theme = prefersLight ? 'light' : 'dark';
+    localStorage.setItem('theme', theme);
+  }
+
+  return theme;
+}
+
+export function toggleTheme() {
+  const currentTheme = getTheme();
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  localStorage.setItem('theme', newTheme);
+  applyTheme(newTheme);
+  return newTheme;
+}
