2fabcac1e5472468d3544327e9cdc4378bdc7e6e
fichince <acchoi@gmail.com>
2023-07-10 15:54:04 -0400

    refactor to Svelte component

---
+++ src/components/Comments.svelte
@@ -0,0 +1,27 @@
+<svelte:head>
+  <script async lang="javascript">
+    var remark_config = {
+      host: 'https://remark.alchoi.com',
+      site_id: 'remark',
+      components: ['embed'],
+      show_rss_subsription: false,
+      theme: 'dark',
+    };
+
+    console.log('remark_config', remark_config);
+
+    !(function (e, n) {
+      for (var o = 0; o < e.length; o++) {
+        var r = n.createElement("script"),
+          c = ".js",
+          d = n.head || n.body;
+        "noModule" in r ? ((r.type = "module"), (c = ".mjs")) : (r.async = !0),
+          (r.defer = !0),
+          (r.src = remark_config.host + "/web/" + e[o] + c),
+          d.appendChild(r);
+      }
+    })(remark_config.components || ["embed"], document);
+  </script>
+</svelte:head>
+
+<div id="remark42" />
