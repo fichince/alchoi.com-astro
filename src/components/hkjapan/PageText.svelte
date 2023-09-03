e58e1d8965276173c2bae74d43308bfebf63a70a
fichince <acchoi@gmail.com>
2023-09-03 14:43:22 -0400

    refactor the basic text part

---
+++ src/components/hkjapan/PageText.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import { renderMarkdown } from '@src/markdown';
+  import { mapMoving } from '@src/stores/map';
+
+  export let title : string = '';
+  export let body : string;
+
+  export let top : string = 'auto';
+  export let bottom : string = 'auto';
+  export let left : string = 'auto';
+  export let right : string = 'auto';
+  export let width : string = 'max-content';
+  export let height : string = 'max-content';
+
+  let show : boolean = false;
+  $: show = !$mapMoving;
+
+  const html = renderMarkdown(body ?? '');
+</script>
+
+<article id="text" class:show class="tw-prose" 
+  style:--_top={top}
+  style:--_bottom={bottom}
+  style:--_left={left}
+  style:--_right={right}
+  style:--_width={width}
+  style:--_height={height}
+>
+  {#if title}
+    <h1>{title}</h1>
+  {/if}
+
+  {#if body}
+    <p>
+      {@html html}
+    </p>
+  {/if}
+</article>
+
+<style lang="scss">
+  article#text {
+    position: absolute;
+
+    // these dimensions are provided by the props of the component
+    top: var(--_top);
+    bottom: var(--_bottom);
+    left: var(--_left);
+    right: var(--_right);
+
+    width: var(--_width);
+    height: var(--_height);
+
+    background-color: var(--colour-background);
+    padding: var(--size-fluid-2);
+    border-radius: var(--radius-2);
+
+    transition: opacity 250ms var(--ease-in-out-1);
+    &.show {
+      opacity: 1;
+    }
+
+    &:not(.show) {
+      opacity: 0;
+    }
+  }
+</style>
