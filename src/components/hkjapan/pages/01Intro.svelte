9ea797be3eb3ae910163288bd8c7a63410095ff3
fichince <acchoi@gmail.com>
2023-09-03 14:43:14 -0400

    refactor the basic text part

--- src/components/hkjapan/pages/01Intro.svelte
+++ src/components/hkjapan/pages/01Intro.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { mapStore, mapPage, mapMoving } from '@src/stores/map';
-  import { renderMarkdown } from '@src/markdown';
-
-  let show : boolean = false;
+  import { mapStore, mapPage } from '@src/stores/map';
+  import PageText from '../PageText.svelte';
 
   onMount(() => {
     const { map } = $mapPage;
@@ -18,37 +16,11 @@
   });
 
   const { body, title } = $mapPage;
-  const html = renderMarkdown(body ?? '');
-
-  $: show = !$mapMoving;
 
 </script>
 
-<div id="intro" class:show class="tw-prose">
-  <h1>{title}</h1>
-  <p>
-  {@html html}
-  </p>
-</div>
-
-<style lang="scss">
-  #intro {
-    position: absolute;
-    top: 60vh;
-
-    width: var(--size-15);
-    right: var(--size-11);
-
-    background-color: var(--colour-background);
-    padding: var(--size-fluid-2);
-    border-radius: var(--radius-2);
-
-    &.show {
-      visibility: visible;
-    }
-    &:not(.show) {
-      visibility: hidden;
-    }
-  }
-
-</style>
+<PageText {title} body={body ?? ''} 
+  top="60vh" 
+  width="var(--size-15)"
+  right="var(--size-11)"
+/>
