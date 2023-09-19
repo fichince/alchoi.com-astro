7714e48791515925465ac88a8f156aab0d6afbed
fichince <acchoi@gmail.com>
2023-09-19 16:01:01 -0400

    star ferry content

---
+++ src/components/hkjapan/pages/06StarFerry.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { mapPage, moveMap } from '@src/stores/map';
+  import { onMount } from 'svelte';
+  import PageText from '../PageText.svelte';
+  import MapImg from '../MapImg.svelte';
+
+  onMount(() => {
+    moveMap();
+  });
+
+  const { body, title, images = [] } = $mapPage;
+
+</script>
+
+<div id="container">
+  <MapImg src={images[0].image} caption={images[0].caption}
+    id={images[0].id} />
+  <MapImg src={images[1].image} caption={images[1].caption}
+    id={images[1].id} />
+</div>
+<PageText {title} {body} 
+  bottom="15vh"
+  left="8vw"
+  width="20vw"
+/>
+
+<style lang="scss">
+  #container {
+
+    position: absolute;
+    top: 50%;
+    right: 8vw;
+    transform: translate(0, -50%);
+
+    display: flex;
+    flex-direction: column;
+    gap: var(--size-3);
+
+    :global(.map-img) {
+      height: 25vh;
+    }
+  }
+</style>
---
+++ src/content/hkjapan/06-star-ferry.md
@@ -0,0 +1,12 @@
+---
+component: 06StarFerry
+map:
+  lat: 22.288933478593336
+  lon: 114.1703557853008
+  zoom: 13
+images:
+  - image: ../../images/hkjapan/06-star-ferry/starferry1.jpg
+  - image: ../../images/hkjapan/06-star-ferry/starferry2.jpg
+---
+
+The Star Ferry is an iconic Hong Kong transportation. Going across the harbour between Kowloon and HK Island.
