fa6945ca5522f93106acfffdf367c0e889949eec
fichince <acchoi@gmail.com>
2024-07-01 16:16:40 -0400

    generating search index JSON

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,13 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
-//import search from './src/integrations/search.ts';
+import search from './src/integrations/search.ts';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alchoi.com',
   output: 'hybrid',
-  integrations: [svelte(), tailwind(), mdx(), sitemap()],
+  integrations: [svelte(), tailwind(), mdx(), sitemap(), search()],
   markdown: {
     shikiConfig: {
       theme: 'vitesse-dark',
--- src/integrations/search.ts
+++ src/integrations/search.ts
@@ -1,14 +1,68 @@
-import type { AstroIntegration } from 'astro';
+import type { AstroIntegration, AstroIntegrationLogger } from 'astro';
+import isString from 'lodash/isString';
 import { fileURLToPath } from 'node:url';
-import { writeFile } from 'node:fs/promises';
-import { getAllBlogEntries } from '../utils';
+import { writeFile, readdir, readFile } from 'node:fs/promises';
+import { basename } from 'node:path';
+//import { getAllBlogEntries } from '../utils';
+//import { getCollection } from 'astro:content';
+import matter from 'gray-matter';
+import { remark } from 'remark';
+import strip from 'strip-markdown';
+
 import Fuse from 'fuse.js';
 
-async function buildIndex() {
-  const blogEntries = await getAllBlogEntries();
-  const index = Fuse.createIndex([ 'title', 'description', 'content' ], blogEntries);
-  return index;
+function stripMarkdown(s? : string) : string {
+  if (!s) return '';
+  if (!isString(s)) return s;
+  return remark().use(strip).processSync(s).toString().trim();
 }
+
+function declutter(s : string) : string {
+  return s.replace(/letterboxd link/, '').replace(/storygraph link/, '').trim();
+}
+
+async function readDirectoryContents(dir : string) : Promise<any> {
+  const srcDir = await readdir(`./src/content/${dir}`);
+
+  const result = [];
+
+  for (let file of srcDir) {
+    if (file.endsWith('.md')) {
+      const fileContent = await readFile(`./src/content/${dir}/${file}`, 'utf8');
+      const { data, content: body } = matter(fileContent);
+
+      const title = stripMarkdown(data.title);
+      const description = stripMarkdown(data.description);
+      const content = declutter(stripMarkdown(body));
+
+      const slug = basename(file, '.md');
+      const urlPath = dir === 'blog' ? '/blog' : '/quick-reviews';
+
+      result.push({
+        title,
+        description,
+        content,
+        url: `${urlPath}/${slug}`,
+      });
+    }
+  }
+
+  return result;
+
+}
+
+async function buildIndex(logger : AstroIntegrationLogger) {
+
+  const blog = await readDirectoryContents('blog');
+  const capsules = await readDirectoryContents('capsules');
+
+  const entries = [...blog, ...capsules];
+  logger.info(`Indexing ${entries.length} entries`);
+  const index = Fuse.createIndex([ 'title', 'description', 'content' ], entries);
+
+  return { entries, index };
+}
+
 
 export default function search(): AstroIntegration {
   return {
@@ -16,13 +70,21 @@ export default function search(): AstroIntegration {
     hooks: {
       'astro:server:start': async ({ logger }) => {
         logger.info('Building search index - dev');
-        const index = await buildIndex();
+        const { index, entries } = await buildIndex(logger);
+
+        await writeFile('./.search/search-index.json', JSON.stringify(index));
+        await writeFile('./.search/search-entries.json', JSON.stringify(entries));
       },
-      'astro:build:done': async ({ dir, logger }) => {
+      'astro:build:done': async (stuff) => {
+
+        const { logger, dir } = stuff;
         logger.info('Building search index');
-        const index = await buildIndex();
-        const outFile = fileURLToPath(new URL('./search-index.json', dir));
-        await writeFile(outFile, JSON.stringify(index));
+
+        const { index, entries } = await buildIndex(logger);
+        const indexOut = fileURLToPath(new URL('./search-index.json', dir));
+        await writeFile(indexOut, JSON.stringify(index));
+        const entriesOut = fileURLToPath(new URL('./search-entries.json', dir));
+        await writeFile(entriesOut, JSON.stringify(entries));
       }
     },
   };
