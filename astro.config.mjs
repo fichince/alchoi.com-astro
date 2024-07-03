ebb263424c3b6a5ee50a36e9002194228e36174d
fichince <acchoi@gmail.com>
2024-07-03 14:07:49 -0400

    experimenting

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,5 +17,8 @@ export default defineConfig({
       wrap: true
     }
   },
-  adapter: vercel()
+  adapter: vercel({
+    functionPerRoute: true,
+    includeFiles: [ '.search/**/*' ],
+  })
 });
