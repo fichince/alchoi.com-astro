129c5acfe2d15e304a8f0979c5ada33ee963924e
fichince <acchoi@gmail.com>
2024-12-04 15:25:09 -0500

    most basic implementation of the loader

--- src/loaders/quoteshelf.ts
+++ src/loaders/quoteshelf.ts
@@ -1,9 +1,32 @@
 import type { LoaderContext, Loader } from 'astro/loaders';
 import { z } from 'astro:content';
+import { readdir, readFile } from 'node:fs/promises';
+import { basename } from 'node:path';
+import { parse } from 'yaml';
 
-export default function quoteshelfLoader(): Loader {
+export default function quoteshelfLoader(options: { base: string }): Loader {
 
   async function load(context: LoaderContext) {
+    const { logger, store, parseData } = context;
+    logger.info('Running the quoteshelf loader');
+
+    const dir = await readdir(options.base);
+
+    for (let file of dir) {
+      if (file.endsWith('.yaml')) {
+        const fileContent = await readFile(`${options.base}/${file}`, 'utf8');
+        const id = basename(file, '.yaml');
+        logger.info('Parsing data for ' + id);
+        const data = parse(fileContent);
+
+        const parsed = await parseData({ id, data });
+
+        store.set({
+          id,
+          data: parsed
+        });
+      }
+    }
   }
 
   const schema = z.object({
