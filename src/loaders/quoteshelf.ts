9339dc42d6255d43260402d0e8d044f5caf59662
fichince <acchoi@gmail.com>
2025-01-02 10:22:07 -0500

    Fixes for quoteshelf (#46)
    * fixing randomizer sizing
    
    * don't prerender randomizer page
    
    * adding some profiling
    
    * using a server island for the randomizer page
    
    * actually server island doesn't work in this case
    
    * dev code for testing delay
    
    * refactor quoteshelf loader to produce individual quotes
    
    * updating randomizer
    
    * tweaking when and how many the randomizer loads
    
    * using Google Books API to get covers (performs faster)
    
    * disable fetching cover for now
    
    * adding coolbackground
    
    * fix up quoteshelf data files
    
    * using Google Books URLs directly without API
    
    * adding image sources
    
    * adding image sources

--- src/loaders/quoteshelf.ts
+++ src/loaders/quoteshelf.ts
@@ -2,13 +2,88 @@ import type { LoaderContext, Loader } from 'astro/loaders';
 import { z } from 'astro:content';
 import { readdir, readFile } from 'node:fs/promises';
 import { basename } from 'node:path';
+import * as crypto from 'node:crypto';
 import { parse } from 'yaml';
 import qs from 'query-string';
+import slugify from '@sindresorhus/slugify';
+import pickBy from 'lodash/pickBy';
+import type { AstroIntegrationLogger } from 'astro';
 
 export default function quoteshelfLoader(options: { base: string }): Loader {
 
+  const inputSchema = z.object({
+    title: z.string(),
+    author: z.string(),
+    sortName: z.string().optional(),
+    quotes: z.string().array(),
+    googleId: z.string().optional()
+  });
+
+  const outputSchema = z.object({
+    title: z.string(),
+    titleSlug: z.string(),
+    author: z.string(),
+    authorSlug: z.string(),
+    sortName: z.string().optional(),
+    quote: z.string(),
+    coverUrl: z.string().optional(),
+  });
+
+  type InputData = z.infer<typeof inputSchema>;
+  type OutputData = z.infer<typeof outputSchema>
+
+  /*
+  async function fetchCover(title: string, author: string) : Promise<string> {
+    const url = qs.stringifyUrl({
+      url: 'https://openlibrary.org/search.json',
+      query: {
+        author: author,
+        q: title,
+      }
+    });
+
+    const response = await fetch(url);
+    const info = await response.json();
+
+    const coverId = info.docs[0]?.cover_i || null;
+
+    if (coverId) {
+      return `https://covers.openlibrary.org/b/id/${coverId}-M.jpg`;
+    } else {
+      return null;
+    }
+  }
+  */
+
+  async function fetchCover(title: string, author: string, logger: AstroIntegrationLogger) : Promise<string> {
+    /*
+    const url = qs.stringifyUrl({
+      url: 'https://www.googleapis.com/books/v1/volumes',
+      query: {
+        key: import.meta.env.GOOGLE_BOOKS_API_KEY,
+        q: `intitle:${title} inauthor:${author}`
+      }
+    });
+
+    const response = await fetch(url);
+    const info = await response.json();
+
+    const coverUrl = info.items?.[0]?.volumeInfo?.imageLinks?.thumbnail;
+
+    if (coverUrl) {
+      logger.info(`Found cover URL for ${title} - ${coverUrl}`);
+    } else {
+      logger.info(`Cover not found for ${title}`);
+    }
+
+    return coverUrl;
+    */
+    return null;
+  }
+
   async function load(context: LoaderContext) {
     const { logger, store, parseData, generateDigest } = context;
+    store.clear();
     logger.info('Running the quoteshelf loader');
 
     const dir = await readdir(options.base);
@@ -18,60 +93,62 @@ export default function quoteshelfLoader(options: { base: string }): Loader {
         const fileContent = await readFile(`${options.base}/${file}`, 'utf8');
         const id = basename(file, '.yaml');
         logger.info('Loading quotes for ' + id);
-        const data = parse(fileContent);
 
-        const digest = generateDigest(data);
-        const existing = store.get(id);
+        const data = parse(fileContent) as InputData;
 
-        if (digest === existing?.digest) {
-          logger.info('Reusing cached data for ' + id);
-          continue;
-        }
+        inputSchema.parse(data);
+        const { title, author, sortName, googleId, quotes } = data;
 
-        try {
-          const url = qs.stringifyUrl({
-            url: 'https://openlibrary.org/search.json',
+        for (let q of quotes) {
+
+          const titleSlug = slugify(title);
+          const authorSlug = slugify(author);
+
+          const coverUrl = Boolean(googleId) ? qs.stringifyUrl({
+            url:  'https://books.google.com/books/content',
             query: {
-              author: data.author,
-              q: data.title,
+              id: googleId,
+              printsec: 'frontcover',
+              img: 1,
+              zoom: 2,
             }
+          }) : null;
+
+          const quote: OutputData = pickBy({
+            title,
+            titleSlug,
+            author,
+            authorSlug,
+            sortName,
+            quote: q,
+            coverUrl,
           });
 
-          const response = await fetch(url);
-          const info = await response.json();
+          const id = crypto.hash('sha-1', q, 'hex');
+          /*
+          const digest = generateDigest(quote);
+          const existing = store.get(id);
 
-          const coverId = info.docs[0]?.cover_i || null;
-
-          if (coverId) {
-            data.coverUrl = `https://covers.openlibrary.org/b/id/${coverId}-M.jpg`;
+          if (digest === existing?.digest) {
+            logger.info('Reusing cached data for ' + id);
+            continue;
           }
+          */
 
-        } catch (e) {
-          logger.warn(`Error fetching cover for ${data.title} - ${e}`);
+          const parsed = await parseData({ id, data: quote });
+          store.set({
+            id,
+            data: parsed,
+            //digest
+          });
         }
-
-        const parsed = await parseData({ id, data });
-
-        store.set({
-          id,
-          data: parsed,
-          digest
-        });
       }
     }
   }
 
-  const schema = z.object({
-    title: z.string(),
-    author: z.string(),
-    sortName: z.string().optional(),
-    quotes: z.string().array(),
-    coverUrl: z.string().optional(),
-  });
-
   return {
     name: 'quoteshelfLoader',
     load,
-    schema,
+    schema: outputSchema,
   };
 }
