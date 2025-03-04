a2d3a2ee90368b071b77a7d24a97d0df3fb4ba3f
fichince <acchoi@gmail.com>
2025-03-04 11:03:38 -0500

    handling inline images and other fixes

--- src/utils.ts
+++ src/utils.ts
@@ -1,66 +1,8 @@
 import { type CollectionEntry, getCollection } from 'astro:content';
-import union from 'lodash/union';
+import padStart from 'lodash/padStart';
 import countBy  from 'lodash/countBy';
 import { DateTime } from 'luxon';
 export const PAGE_SIZE = 10;
-
-export function enrichCapsuleEntry(entry : CollectionEntry<'capsules'>)
-  : CollectionEntry<'capsules'> {
-
-  const { type } = entry.data;
-  const tags = ['quick reviews', ...(entry.data.tags ?? [])];
-  switch (type) {
-    case 'book':
-      tags.push('books');
-      break;
-    case 'movie':
-      tags.push('movies');
-      break;
-    case 'tv':
-      tags.push('tv');
-      break;
-    case 'game':
-      tags.push('games');
-      break;
-    case 'music':
-      tags.push('music');
-      break;
-  }
-
-  return {
-    ...entry,
-    data: {
-      ...entry.data,
-      tags
-    }
-  }
-}
-
-export async function getAllBlogEntries() : Promise<BlogEntry[]> {
-
-  const [ blog, capsules ] = await Promise.all([
-    getCollection('blog'),
-    getCollection('capsules'),
-  ]);
-
-  const published = [...blog, ...capsules].filter((post) => {
-    return !post.data.draft;
-  });
-
-  const entries : BlogEntry[] = published.map((entry) => {
-    if (entry.collection === 'capsules') {
-      return enrichCapsuleEntry(entry);
-    } else {
-      return entry;
-    }
-  });
-
-  return entries.sort((a, b) => {
-    const dateA = a.data.end ?? a.data.date;
-    const dateB = b.data.end ?? b.data.date;
-    return dateB.valueOf() - dateA.valueOf()
-  });
-}
 
 export async function getCMSBlogEntries(): Promise<CollectionEntry<'cmsBlog'>[]> {
   const collection = await getCollection('cmsBlog');
@@ -85,10 +27,12 @@ export function mediumDate(d : string) : string {
     .toLocaleString(DateTime.DATE_MED);
 }
 
-export function getLinkToPost(post? : BlogCollectionEntry | CapsuleCollectionEntry) : string {
-  if (!post) return '';
-  const path = post.collection === 'blog' ? '/blog' : '/quick-reviews';
-  const link = `${path}/${post.id}`;
+export function getLinkToPost(post : CollectionEntry<'cmsBlog'>) : string {
+  const date = DateTime.fromISO(post.data.date);
+  const year = date.year
+  const month = padStart(`${date.month}`, 2, '0');
+
+  const link = `/blog/${year}/${month}/${post.id}`;
   return link;
 }
 
