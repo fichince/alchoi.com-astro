f54ad835dc841a3a12a74576af256bdf82b9b4d5
fichince <acchoi@gmail.com>
2025-11-20 14:18:23 -0500

    set up for keystatic and first test post

---
+++ keystatic.config.ts
@@ -0,0 +1,50 @@
+import { config, fields, collection } from '@keystatic/core';
+
+export default config({
+  storage: {
+    kind: 'local',
+  },
+  collections: {
+    blog: collection({
+      label: 'Blog',
+      slugField: 'title',
+      path: 'src/data/ks-blog/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        description: fields.text({ label: 'Description' }),
+        content: fields.markdoc({ 
+          label: 'Content',
+          extension: 'md',
+          options: {
+            image: {
+              directory: 'src/assets/images/ks-blog',
+              publicPath: '@assets/images/ks-blog',
+            }
+          }
+        }),
+        date: fields.date({ label: 'Date', validation: { isRequired: true } }),
+        start: fields.date({ label: 'Start Date' }),
+        image: fields.image({ 
+          label: 'Image',
+          directory: 'src/assets/images/ks-blog',
+          publicPath: '@assets/images/ks-blog',
+        }),
+        tags: fields.array(
+          fields.text({ label: 'Tag' }),
+          { 
+            label: 'Tags',
+            itemLabel: props => props.value
+          }
+        ),
+        author: fields.text({ label: 'Author' }),
+        rating: fields.number({ 
+          label: 'Rating', 
+          validation: { min: 1, max: 5 },
+          step: 0.5,
+        }),
+        draft: fields.checkbox({ label: 'Draft' }),
+      },
+    }),
+  },
+});
---
+++ src/data/ks-blog/foo.md
@@ -0,0 +1,18 @@
+---
+title: foo
+date: 2025-11-20
+image: '@assets/images/ks-blog/foo/image.png'
+tags:
+  - tag2
+  - tag2
+draft: false
+---
+this is the content
+
+*with some markdown*
+
+<!– excerpt -->
+
+some stuff below the line
+
+![](@assets/images/ks-blog/foo/1se-thumb-2025-05.jpg)
